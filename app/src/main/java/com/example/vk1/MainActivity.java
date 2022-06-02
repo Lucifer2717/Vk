bcb7d3ff41e79194ebd2ffb476954d72b70cf17e
Vedant <vedantkhodwe1801@gmail.com>
2022-06-02 18:16:06 +0530

    added comment

--- app/src/main/java/com/example/vk1/MainActivity.java
+++ app/src/main/java/com/example/vk1/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    //checking for github....
     }
 
     public void totext(View view) {
